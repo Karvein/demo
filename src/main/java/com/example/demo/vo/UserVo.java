ee0e59f736d9d13aeefd1011e9a7a0d9712f3934
Karvein <bkzwei201@gmail.com>
2022-03-30 15:01:51 +0800

    Description: UserVo adjust some orders;

--- src/main/java/com/example/demo/vo/UserVo.java
+++ src/main/java/com/example/demo/vo/UserVo.java
@@ -11,8 +11,8 @@ import org.springframework.stereotype.Component;
 public class UserVo {
     // VO层将部分不返回给前端的值过滤，保证安全性
     // 将JSONField(name= "???", ordinal = ?), name为前端展示名, ordinal为展现次序
-    @JSONField(name = "用户名", ordinal = 2)
+    @JSONField(name = "用户名", ordinal = 1)
     public String user_name;
-    @JSONField(name = "用户密码", ordinal = 3)
+    @JSONField(name = "用户密码", ordinal = 2)
     private String user_password;
 }
