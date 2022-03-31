607cc81d8c6918be7954bfcc57cf8c1c811585e7
Karvein <bkzwei201@gmail.com>
2022-03-31 09:25:12 +0800

    small change;

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -3,7 +3,6 @@ package com.example.demo.controller;
 import com.alibaba.fastjson.JSON;
 import com.example.demo.Service.impl.UserServiceImpl;
 import com.example.demo.pojo.User;
-import com.example.demo.vo.UserVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
