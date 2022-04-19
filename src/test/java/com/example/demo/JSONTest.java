57987a182f70d1904593b6da13029da4176b82e4
Karvein <bkzwei201@gmail.com>
2022-04-19 09:33:24 +0800

    解析Json和Gson的对象转化为String对象

--- src/test/java/com/example/demo/JSONTest.java
+++ src/test/java/com/example/demo/JSONTest.java
@@ -13,7 +13,7 @@ public class JSONTest {
     User user;
 
     @Test
-    public void Gsontest(){
+    public void gsonTest(){
 //      利用Gson实现将一个JSON转化为实体类
         String jsonIn = "{\"user_id\":30,\"user_name\":\"Joker\",\"user_password\":\"useP\",\"create_time\":\"2022-04-18 16:55:00\",\"update_time\":\"2022-04-18 16:55:00\",\"deleted\":0}";
 //        Gson gson = new Gson();
@@ -31,7 +31,7 @@ public class JSONTest {
     }
 
     @Test
-    public void Jsontest(){
+    public void jsonTest(){
 //      利用Gson实现将一个JSON转化为实体类
         String jsonIn = "{\"user_id\":30,\"user_name\":\"Joker\",\"user_password\":\"useP\",\"create_time\":\"2022-04-18 16:55:00\",\"update_time\":\"2022-04-18 16:55:00\",\"deleted\":0}";
 //        Gson gson = new Gson();
@@ -49,6 +49,21 @@ public class JSONTest {
 
     }
 
+
+    @Test
+    public void test() {
+        // 声明user,未初始化,进行内存分配,user无空间,产生空指针异常
+        user = new User();
+        user.setUser_id(25);
+        user.setUser_name("Korn");
+        user.setUser_password("kuiwf86");
+        user.setDeleted(0);
+        user.setCreate_time(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss z").format(new Date(System.currentTimeMillis())));
+        user.setUpdate_time(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss z").format(new Date(System.currentTimeMillis())));
+        System.out.println(JSON.toJSONString(user));
+        Gson gson = new Gson();
+        System.out.println(gson.toJson(user));
+    }
 //    @Test
 //    public void time(){
 //        SimpleDateFormat formatter= new SimpleDateFormat("yyyy-MM-dd 'at' HH:mm:ss z");
